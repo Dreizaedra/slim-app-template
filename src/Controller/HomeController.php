46ee96c3907dd989456b1d038ec8ea325bb8ce39
Louis Arnault <louisarnault@gmail.com>
2024-02-23 09:02:45 +0100

    Change to 'more template-y' files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,24 @@
 <?php
 
 namespace App\Controller;
+
+use Exception;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use App\Model\Customer;
 
 class HomeController extends Controller {
 
     public function index(Request $request, Response $response) {
+        $res['status'] = 'error';
+        try {
+            // Your code here
+            $res['status'] = 'success';
+        } catch (Exception $e) {
+            $res['error'] = $e->getMessage();
+        }
         echo $this->render('home/home.html.twig', [
-            'res' => [
-                'password' => password_hash('secret', PASSWORD_BCRYPT)
-            ]
+            'res' => $res
         ]);
-        return $response->withStatus(200);
+        return $response;
     }
 }
